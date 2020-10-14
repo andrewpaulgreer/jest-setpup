a63ba29fa735fed787832277e7fd0e04a251dfaa
Andrew Greer <62513150+andrewpaulgreer@users.noreply.github.com>
2020-10-14 14:49:24 -0400

    commit ui start, about to start testing

---
+++ src/components/header/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './styles.scss'
+
+function Header() {
+    return (
+       <header>
+           <div className="wrap">
+               <div className="logo">
+                   <h1 className="logo-text">LOGO</h1>
+               </div>
+           </div>
+       </header>
+    )
+}
+
+export default Header
